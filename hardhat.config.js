5149f4de122a47b08d4e23f912c424e9dfc073f8
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-19 22:29:46 +0530

    some basic setup

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,30 @@
 require("@nomicfoundation/hardhat-toolbox");
 
+require("dotenv").config();
 /** @type import('hardhat/config').HardhatUserConfig */
+
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
 module.exports = {
-  solidity: "0.8.24",
+  solidity: "0.7.6",
+  networks: {
+    sepolia: {
+      url: "https://sepolia.infura.io/v3/006a677fe90346f9bf6cb52a2a6b340b",
+      accounts: [PRIVATE_KEY],
+    },
+    hardhat: {
+      chainId: 31337,
+    },
+    zkEVM: {
+      url: "https://rpc.cardona.zkevm-rpc.com",
+      accounts: [PRIVATE_KEY],
+    },
+    scrollSepolia: {
+      url: "https://sepolia-rpc.scroll.io/",
+      accounts: [PRIVATE_KEY],
+    },
+    BNB_testnet: {
+      url: "https://data-seed-prebsc-1-s1.binance.org:8545",
+      accounts: [PRIVATE_KEY],
+    },
+  },
 };
--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,7 +1,9 @@
 const hre = require("hardhat");
 
 async function main() {
-  const Create = await hre.ethers.deployContract("MultiTokenSwapper");
+  const Create = await hre.ethers.deployContract("MultiTokenSwapper", [
+    "0x3bFA4769FB09eefC5a80d6E87c3B9C650f7Ae48E",
+  ]);
   await Create.waitForDeployment();
 
   console.log("contract Address:", Create.target);
