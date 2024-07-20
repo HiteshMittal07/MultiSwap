105788d7ebff5225cc67a4af3deb5a4d2ad6ffce
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-07-20 15:30:27 +0530

    some changes

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -3,6 +3,7 @@ const hre = require("hardhat");
 async function main() {
   const Create = await hre.ethers.deployContract("MultiSwap", [
     "0x4ce3a7A6CAdbE423Fb90339813023A6c69a92CbC",
+    "0x0227628f3F023bb0B980b67D528571c95c6DaC1c",
   ]);
   await Create.waitForDeployment();
 
