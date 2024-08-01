76bb0b1c60bbee45c8300a5cf32160a4d5cf3609
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-08-01 11:43:10 +0530

    test progressing

--- test/swap.js
+++ test/swap.js
@@ -1,6 +1,5 @@
 const { expect } = require("chai");
 const { ethers } = require("hardhat");
-
 describe("MultiSwap", function () {
   let Contract;
   let contract;
@@ -8,15 +7,18 @@ describe("MultiSwap", function () {
   let token_contract2;
   let output_token;
 
-  it("swap", async function () {
-    const provider = new ethers.providers.JsonRpcProvider(
-      "https://polygon-mainnet.infura.io/v3/6ee38d8487b64702a7736903e3e8e690"
-    );
+  this.beforeAll(async function () {
     Contract = await ethers.getContractFactory("MultiSwap");
     contract = await Contract.deploy(
       "0xE592427A0AEce92De3Edee1F18E0157C05861564"
     );
     await contract.waitForDeployment();
+  });
+  it("swap", async function () {
+    const provider = new ethers.providers.JsonRpcProvider(
+      "https://polygon-mainnet.infura.io/v3/6ee38d8487b64702a7736903e3e8e690"
+    );
+    console.log(contract);
     const selectedTokens = [
       "0x53E0bca35eC356BD5ddDFebbD1Fc0fD03FaBad39",
       "0x7ceB23fD6bC0adD59E62ac25578270cFf1b9f619",
@@ -24,9 +26,17 @@ describe("MultiSwap", function () {
     const tokenABI = [
       "function approve(address spender, uint256 amount) external returns (bool)",
     ];
-    token_contract1 = new ethers.Contract(tokenAddress, tokenABI, provider);
-
+    const signer = provider.getSigner();
+    // console.log(signer);
+    token_contract1 = new ethers.Contract(
+      selectedTokens[0],
+      tokenABI,
+      provider
+    );
+    const token_contract1_signer = token_contract1.connect(signer);
+    console.log(token_contract1_signer);
     const amountsIn = [100000000000, 1000000000];
+    token_contract1_signer.approve();
     const outputToken = "0xb33EaAd8d922B1083446DC23f610c2567fB5180f";
     const tx = await contract.multiSwapExactInputSingle(
       selectedTokens,
